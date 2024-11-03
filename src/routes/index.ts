3f7f4a76ef6dcea89958b49130c915abc8112859
soo essoklina ulrich <essoklina.soo@ipnetinstitute.com>
2024-11-03 08:21:50 +0000

    Remove unused home route from root router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,7 @@
-import {NextFunction, Request, Response, Router} from 'express';
+import {Router} from 'express';
 import CategorieRouter from './categorieRoute/categorie';
 const rootRouter:Router =  Router();
 
-/* GET home page. */
-rootRouter.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.render('index', { title: 'Express' });
-});
-
 rootRouter.use('/cat', CategorieRouter);
-
-
 
 export default rootRouter;
