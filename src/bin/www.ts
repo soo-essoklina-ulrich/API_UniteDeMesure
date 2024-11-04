990261e37bda37af1805cf50951239784981f4ba
soo essoklina ulrich <essoklina.soo@ipnetinstitute.com>
2024-11-04 10:38:41 +0000

    Refactor www.ts: standardize quotes, remove dotenv configuration, and improve logging messages

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -4,19 +4,19 @@
  * Module dependencies.
  */
 
-import app from '../app';
-import http from 'http';
-import dotenv from 'dotenv';
-var debug = require('debug')('unite:server');
+import app from "../app";
+import http from "http";
+// import dotenv from 'dotenv';
+var debug = require("debug")("unite:server");
 
-dotenv.config({path: '.env'});
+// dotenv.config({path: '.env'});
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '8000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "8000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -29,10 +29,10 @@ var server = http.createServer(app);
  */
 
 server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -59,22 +59,20 @@ function normalizePort(val: string) {
  */
 
 function onError(error: any) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -88,8 +86,6 @@ function onError(error: any) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr!.port;
-  debug(`Listening on  + ${addr}:${bind}`);
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr!.port;
+  debug(`Listening on  port ${bind}`);
 }
